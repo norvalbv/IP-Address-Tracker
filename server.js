71a279173073a9a288ff28a94706e195bb06500f
norvalbv <benjinorval@gmail.com>
2022-01-17 13:57:40 +0000

    attempt to fix heroku

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { stage, port, keyMap, keyID } = require('./config');
+const { port } = require('./config');
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('hi');
+});
 
 app.listen(port, () => {
     console.log(`App is listening on ${port}.`);
