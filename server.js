07cdb8f9eda3c0f4e3942ae05ec2120044847ef0
norvalbv <benjinorval@gmail.com>
2022-01-18 00:38:59 +0000

    minor changes

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const dotenv = require('dotenv');
 const result = dotenv.config();
 
-if (result.error) {
-    throw result.error;
-}
+// if (result.error) {
+//     throw result.error;
+// }
 
 const PORT = process.env.PORT || 3000;
 
