069a206c3bc7dc1ee9f459245e9aed5882d316a5
norvalbv <benjinorval@gmail.com>
2022-01-18 15:22:36 +0000

    minor changes

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
-const result = dotenv.config();
-const path = require('path')
 
-// if (result.error) {
-//     throw result.error;
-// }
-
-const PORT = process.env.PORT || 3000;
+const path = require('path');
+const { port } = require('./config');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-    console.log(`App is listening on ${PORT}.`);
+app.listen(port, () => {
+    console.log(`App is listening on ${port}.`);
 });
